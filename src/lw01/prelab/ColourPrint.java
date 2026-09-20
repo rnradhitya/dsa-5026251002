d0f9ef44e2f999ea64e0b9825f83d5410f1a5c6f
rnradhitya <radhitya200874@gmail.com>
2026-09-20 11:07:09 +0700

    Labwork 01 Prelab

---
+++ src/lw01/prelab/ColourPrint.java
@@ -0,0 +1,25 @@
+package lw01.prelab;
+
+public class ColourPrint extends PrintJob {
+
+    public ColourPrint(String id, int pages) {
+        super(id, pages);
+    }
+
+    @Override
+    public int calculateCharge() {
+        int pages = getPages();
+        int cost;
+        if (pages <= 10) {
+            cost = pages * 1500;
+        } else {
+            cost = (10 * 1500) + ((pages - 10) * 1000);
+        }
+        return cost + 2000;
+    }
+
+    @Override
+    public String label() {
+        return "Colour";
+    }
+}
